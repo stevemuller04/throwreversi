07f7ad9bb6665aa65a1f0e34cd15abd7638320c2
Steve Muller <steve.muller@outlook.com>
2018-11-19 22:35:10 +0100

    Fix: for consistency, GodmodeGame::playerMove() should always return true

--- src/Game/GodmodeGame.cpp
+++ src/Game/GodmodeGame.cpp
@@ -15,21 +15,18 @@ bool GodmodeGame::playerMove(Board &board, Player player, Tile const &tile, Tile
 {
 	// In god mode, we only change the owner of the given tile; there is no further game logic
 
-	// Check if the owner is really changed
 	Player const owner = board.getTileOwner(tile);
 	if (owner != player)
 	{
-		// Change the tile owner
 		board.setTileOwner(tile, player);
-
-		// There is only one tile update 
-		num_updates = 1;
-		updates[0].tile = tile;
-		updates[0].owner = player;
-		updates[0].previous_owner = owner;
-
-		return true;
 	}
-	else
-		return false;
+
+	// There is only one tile update; for the sake of consistency with playerCanMove(),
+	// do this change even if it was not really necessary.
+	num_updates = 1;
+	updates[0].tile = tile;
+	updates[0].owner = player;
+	updates[0].previous_owner = owner;
+
+	return true;
 }
