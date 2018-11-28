7ae4efe44096341aa2e96b4638fc824f94fce740
Steve Muller <steve.muller@outlook.com>
2018-11-28 23:12:34 +0100

    Added HTML code of web interface

---
+++ HTML.h
@@ -0,0 +1,143 @@
+R"=====(<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/>
+<style type="text/css">
+/*! normalize.css v3.0.2 | MIT License | git.io/normalize */img,legend{border:0}legend,td,th{padding:0}html{font-family:sans-serif;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,details,figcaption,figure,footer,header,hgroup,main,menu,nav,section,summary{display:block}audio,canvas,progress,video{display:inline-block;vertical-align:baseline}audio:not([controls]){display:none;height:0}[hidden],template{display:none}a{background-color:transparent}a:active,a:hover{outline:0}abbr[title]{border-bottom:1px dotted}b,optgroup,strong{font-weight:700}dfn{font-style:italic}h1{font-size:2em;margin:.67em 0}mark{background:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sup{top:-.5em}sub{bottom:-.25em}svg:not(:root){overflow:hidden}figure{margin:1em 40px}hr{-moz-box-sizing:content-box;box-sizing:content-box;height:0}pre,textarea{overflow:auto}code,kbd,pre,samp{font-family:monospace,monospace;font-size:1em}button,input,optgroup,select,textarea{color:inherit;font:inherit;margin:0}button{overflow:visible}button,select{text-transform:none}button,html input[type=button],input[type=reset],input[type=submit]{-webkit-appearance:button;cursor:pointer}button[disabled],html input[disabled]{cursor:default}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0}input{line-height:normal}input[type=checkbox],input[type=radio]{box-sizing:border-box;padding:0}input[type=number]::-webkit-inner-spin-button,input[type=number]::-webkit-outer-spin-button{height:auto}input[type=search]{-webkit-appearance:textfield;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box}input[type=search]::-webkit-search-cancel-button,input[type=search]::-webkit-search-decoration{-webkit-appearance:none}fieldset{border:1px solid silver;margin:0 2px;padding:.35em .625em .75em}table{border-collapse:collapse;border-spacing:0}
+/* Skeleton V2.0.4 | Copyright 2014, Dave Gamache, www.getskeleton.com | Free to use under the MIT license. */.column,.columns,.container,.u-full-width{width:100%;box-sizing:border-box}h1,h2,h3{letter-spacing:-.1rem}body,h6{line-height:1.6}.container{position:relative;max-width:960px;margin:0 auto;padding:0 20px}ol,p,ul{margin-top:0}.column,.columns{float:left}@media (min-width:400px){.container{width:85%;padding:0}}html{font-size:62.5%}body{font-size:1.5em;font-weight:400;font-family:Raleway,HelveticaNeue,"Helvetica Neue",Helvetica,Arial,sans-serif;color:#222}h1,h2,h3,h4,h5,h6{margin-top:0;margin-bottom:2rem;font-weight:300}h1{font-size:4rem;line-height:1.2}h2{font-size:3.6rem;line-height:1.25}h3{font-size:3rem;line-height:1.3}h4{font-size:2.4rem;line-height:1.35;letter-spacing:-.08rem}h5{font-size:1.8rem;line-height:1.5;letter-spacing:-.05rem}h6{font-size:1.5rem;letter-spacing:0}@media (min-width:550px){.container{width:80%}.column,.columns{margin-left:4%}.column:first-child,.columns:first-child{margin-left:0}.one.column,.one.columns{width:4.66666666667%}.two.columns{width:13.3333333333%}.three.columns{width:22%}.four.columns{width:30.6666666667%}.five.columns{width:39.3333333333%}.six.columns{width:48%}.seven.columns{width:56.6666666667%}.eight.columns{width:65.3333333333%}.nine.columns{width:74%}.ten.columns{width:82.6666666667%}.eleven.columns{width:91.3333333333%}.twelve.columns{width:100%;margin-left:0}.one-third.column{width:30.6666666667%}.two-thirds.column{width:65.3333333333%}.one-half.column{width:48%}.offset-by-one.column,.offset-by-one.columns{margin-left:8.66666666667%}.offset-by-two.column,.offset-by-two.columns{margin-left:17.3333333333%}.offset-by-three.column,.offset-by-three.columns{margin-left:26%}.offset-by-four.column,.offset-by-four.columns{margin-left:34.6666666667%}.offset-by-five.column,.offset-by-five.columns{margin-left:43.3333333333%}.offset-by-six.column,.offset-by-six.columns{margin-left:52%}.offset-by-seven.column,.offset-by-seven.columns{margin-left:60.6666666667%}.offset-by-eight.column,.offset-by-eight.columns{margin-left:69.3333333333%}.offset-by-nine.column,.offset-by-nine.columns{margin-left:78%}.offset-by-ten.column,.offset-by-ten.columns{margin-left:86.6666666667%}.offset-by-eleven.column,.offset-by-eleven.columns{margin-left:95.3333333333%}.offset-by-one-third.column,.offset-by-one-third.columns{margin-left:34.6666666667%}.offset-by-two-thirds.column,.offset-by-two-thirds.columns{margin-left:69.3333333333%}.offset-by-one-half.column,.offset-by-one-half.columns{margin-left:52%}h1{font-size:5rem}h2{font-size:4.2rem}h3{font-size:3.6rem}h4{font-size:3rem}h5{font-size:2.4rem}h6{font-size:1.5rem}}a{color:#1EAEDB}a:hover{color:#0FA0CE}.button,button,input[type=submit],input[type=reset],input[type=button]{display:inline-block;height:38px;padding:0 30px;color:#555;text-align:center;font-size:11px;font-weight:600;line-height:38px;letter-spacing:.1rem;text-transform:uppercase;text-decoration:none;white-space:nowrap;background-color:transparent;border-radius:4px;border:1px solid #bbb;cursor:pointer;box-sizing:border-box}.button:focus,.button:hover,button:focus,button:hover,input[type=submit]:focus,input[type=submit]:hover,input[type=reset]:focus,input[type=reset]:hover,input[type=button]:focus,input[type=button]:hover{color:#333;border-color:#888;outline:0}.button.button-primary,button.button-primary,input[type=submit].button-primary,input[type=reset].button-primary,input[type=button].button-primary{color:#FFF;background-color:#33C3F0;border-color:#33C3F0}.button.button-primary:focus,.button.button-primary:hover,button.button-primary:focus,button.button-primary:hover,input[type=submit].button-primary:focus,input[type=submit].button-primary:hover,input[type=reset].button-primary:focus,input[type=reset].button-primary:hover,input[type=button].button-primary:focus,input[type=button].button-primary:hover{color:#FFF;background-color:#1EAEDB;border-color:#1EAEDB}input[type=tel],input[type=url],input[type=password],input[type=email],input[type=number],input[type=search],input[type=text],select,textarea{height:38px;padding:6px 10px;background-color:#fff;border:1px solid #D1D1D1;border-radius:4px;box-shadow:none;box-sizing:border-box}input[type=tel],input[type=url],input[type=password],input[type=email],input[type=number],input[type=search],input[type=text],textarea{-webkit-appearance:none;-moz-appearance:none;appearance:none}textarea{min-height:65px;padding-top:6px;padding-bottom:6px}input[type=tel]:focus,input[type=url]:focus,input[type=password]:focus,input[type=email]:focus,input[type=number]:focus,input[type=search]:focus,input[type=text]:focus,select:focus,textarea:focus{border:1px solid #33C3F0;outline:0}label,legend{display:block;margin-bottom:.5rem;font-weight:600}fieldset{padding:0;border-width:0}input[type=checkbox],input[type=radio]{display:inline}label>.label-body{display:inline-block;margin-left:.5rem;font-weight:400}ul{list-style:circle inside}ol{list-style:decimal inside}ol,ul{padding-left:0}ol ol,ol ul,ul ol,ul ul{margin:1.5rem 0 1.5rem 3rem;font-size:90%}.button,button,li{margin-bottom:1rem}code{padding:.2rem .5rem;margin:0 .2rem;font-size:90%;white-space:nowrap;background:#F1F1F1;border:1px solid #E1E1E1;border-radius:4px}pre>code{display:block;padding:1rem 1.5rem;white-space:pre}td,th{padding:12px 15px;text-align:left;border-bottom:1px solid #E1E1E1}td:first-child,th:first-child{padding-left:0}td:last-child,th:last-child{padding-right:0}fieldset,input,select,textarea{margin-bottom:1.5rem}blockquote,dl,figure,form,ol,p,pre,table,ul{margin-bottom:2.5rem}.u-max-full-width{max-width:100%;box-sizing:border-box}.u-pull-right{float:right}.u-pull-left{float:left}hr{margin-top:3rem;margin-bottom:3.5rem;border-width:0;border-top:1px solid #E1E1E1}.container:after,.row:after,.u-cf{content:"";display:table;clear:both}
+body.container {
+	margin-top: 3rem;
+	text-align: center;
+	background-color: #eee;
+}
+.boardrow {
+	display: flex;
+}
+.boardrow>div {
+	padding: .5rem;
+	flex: auto;
+}
+.boardrow>div>button {
+	display: block;
+	width: 100%;
+	height: 7rem;
+	margin: 0;
+	border-radius: 4px;
+}
+.blue {
+	background-color: #93C9D9;
+}
+.red {
+	background-color: #FC9D9B;
+}
+.white {
+	background-color: #ffffff;
+}
+.x::after {
+	content: " (✔)";
+}
+</style>
+<script type="text/javascript">
+function ajax(url, method, args, callback)
+{
+	var argstring = "";
+	for (var k in args)
+		argstring += (argstring.length == 0 ? "?" : "&") + k + "=" + encodeURIComponent(args[k]);
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		xhr.readyState === 4 && xhr.status === 200 && callback && callback(JSON.parse(xhr.responseText));
+	};
+	xhr.open(method, url + argstring);
+	xhr.send(null);
+}
+function Game()
+{
+	this.godmode = false;
+	this.board = [];
+	this.built = false;
+	this.player = 0;
+}
+Game.prototype.refresh = function () {
+	var self = this;
+	ajax("/board", "GET", {}, function (data) {
+		self.godmode = data.godmode;
+		self.board = data.board;
+		if (!self.built) {
+			self.buildBoard();
+			self.built = true;
+		}
+		self.updateBoard();
+	});
+};
+Game.prototype.buildBoard = function() {
+	var self = this;
+	var nBoard = document.getElementById("board");
+	for (var y = 0; y < this.board.length; y++)
+	{
+		var nRow = document.createElement("div");
+		nRow.className = "boardrow";
+		for (var x = 0; x < this.board[y].length; x++)
+		{
+			var nCol = document.createElement("div");
+			var nBtn = document.createElement("button");
+			nBtn.setAttribute("type", "button");
+			nBtn.setAttribute("bx", x);
+			nBtn.setAttribute("by", y);
+			nBtn.onclick = function() { self.move(this.getAttribute("bx"), this.getAttribute("by")); };
+			nCol.appendChild(nBtn);
+			nRow.appendChild(nCol);
+		}
+		nBoard.appendChild(nRow);
+	}
+};
+Game.prototype.updateBoard = function() {
+	var nBtns = document.getElementById("board").querySelectorAll("button");
+	for (var i = 0; i < nBtns.length; i++) {
+		var x = nBtns[i].getAttribute("bx");
+		var y = nBtns[i].getAttribute("by");
+		var p = this.board[y][x];
+		nBtns[i].className = p == 1 ? "blue" : p == 2 ? "red" : "white";
+	}
+	var nBtnP1 = document.getElementById("btnP1");
+	var nBtnP2 = document.getElementById("btnP2");
+	var nBtnP0 = document.getElementById("btnP0");
+	var nBtnGM = document.getElementById("btnGM");
+	nBtnP1.className = this.player == 1 ? "blue x" : "";
+	nBtnP2.className = this.player == 2 ? "red x" : "";
+	nBtnP0.className = this.player == 0 ? "white x" : "";
+	nBtnGM.className = this.godmode ? "white x" : "";
+};
+Game.prototype.setPlayer = function(p) {
+	this.player = p;
+	this.updateBoard();
+};
+Game.prototype.move = function(x, y) {
+	var self = this;
+	ajax("/move", "POST", { x: x, y: y, p: this.player }, function() { self.refresh(); });
+};
+Game.prototype.toggleGodMode = function() {
+	var self = this;
+	ajax("/godmode", "POST", {}, function() { self.refresh(); });
+};
+var G = new Game();
+document.addEventListener("DOMContentLoaded", function() { G.refresh(); });
+</script>
+<title>ThrowReversi</title>
+</head>
+<body class="container">
+	<div class="row">
+		<h1>ThrowReversi</h1>
+		<p>Choose a player</p>
+		<p>
+			<button type="button" id="btnP1" onclick="G.setPlayer(1)">blue</button>
+			<button type="button" id="btnP2" onclick="G.setPlayer(2)">red</button>
+			<button type="button" id="btnP0" onclick="G.setPlayer(0)" class="white x">none</button>
+			<span style="margin-left:1rem"></span>
+			<button type="button" id="btnGM" onclick="G.toggleGodMode()">Edit mode</button>
+		</p>
+		<p>Choose a tile</p>
+		<div id="board"></div>
+	</div>
+</body>
+</html>)====="
